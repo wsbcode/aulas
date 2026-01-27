295dc513ecb876dc7dc3fc46ea0f2081f21d027a
William Barbosa <wsbprofissional@gmail.com>
2026-01-26 22:28:31 -0300

    subindo aulas typescript

--- typescript-fundamentos/nescessidade-ts/public/script.js
+++ typescript-fundamentos/nescessidade-ts/public/script.js
@@ -1,16 +1,15 @@
 "use strict";
 // AULAS DE ( TypeScript) , mostrando exemplo do TypeScrip
-Object.defineProperty(exports, "__esModule", { value: true });
 let numero1 = document.getElementById("numero1");
 let numero2 = document.getElementById("numero2");
 let botao = document.getElementById("calcular");
 let resposta = document.getElementById("resultado");
 function somar(n1, n2) {
-   return n1 + n2;
+    return n1 + n2;
 }
 botao.addEventListener("click", function () {
-   let valor1 = Number(numero1.value);
-   let valor2 = Number(numero2.value);
-   let exibir = somar(valor1, valor2);
-   resposta.innerHTML = `A soma entre ${valor1} + ${valor2} é = ${exibir}`;
+    let valor1 = Number(numero1.value);
+    let valor2 = Number(numero2.value);
+    let exibir = somar(valor1, valor2);
+    resposta.innerHTML = `A soma entre ${valor1} + ${valor2} é = ${exibir}`;
 });
