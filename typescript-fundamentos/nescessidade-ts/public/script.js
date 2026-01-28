220d5fef3b11fd40742593342eef97e48137dcda
William Barbosa <wsbprofissional@gmail.com>
2026-01-27 23:59:33 -0300

    Subindo aulas typescript

--- typescript-fundamentos/nescessidade-ts/public/script.js
+++ typescript-fundamentos/nescessidade-ts/public/script.js
@@ -4,12 +4,12 @@ let numero2 = document.getElementById("numero2");
 let botao = document.getElementById("calcular");
 let resposta = document.getElementById("resultado");
 function somar(n1, n2) {
-    return n1 + n2;
+   return n1 + n2;
 }
 botao.addEventListener("click", function () {
-    let valor1 = Number(numero1.value);
-    let valor2 = Number(numero2.value);
-    let exibir = somar(valor1, valor2);
-    resposta.innerHTML = `A soma entre ${valor1} + ${valor2} é = ${exibir}`;
+   let valor1 = Number(numero1.value);
+   let valor2 = Number(numero2.value);
+   let exibir = somar(valor1, valor2);
+   resposta.innerHTML = `A soma entre ${valor1} + ${valor2} é = ${exibir}`;
 });
 export {};
