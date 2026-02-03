08d4b59b13bb9ec84bc65368d2d732917133e2cd
William Barbosa <wsbprofissional@gmail.com>
2026-02-02 22:52:49 -0300

    Aulas TypeScript

---
+++ typescript-fundamentos/public/typesObjetos.js
@@ -0,0 +1,8 @@
+// Cria a instância respeitando a interface definida
+const meuCarro = {
+    marca: "Fiat Uno",
+    modelo: "Com Escada",
+    ano: 2024,
+    // 'flex' foi omitido sem gerar erro por ser opcional
+};
+export {};
